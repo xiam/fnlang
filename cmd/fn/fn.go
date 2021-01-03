5e4f6a1193c71f3c3e859fb3cc20b3f21520445a
José Nieto <jose@xiam.io>
2021-01-03 01:33:08 -0600

    add pipe readwriter

--- cmd/fn/fn.go
+++ cmd/fn/fn.go
@@ -1,44 +1,55 @@
 package main
 
 import (
-	//"bytes"
-	//"fmt"
-	//"github.com/peterh/liner"
-	//"github.com/xiam/fnlang"
-	//_ "github.com/xiam/fnlang/stdlib"
-	//"github.com/xiam/sexpr/parser"
-	"errors"
+	"fmt"
+	"github.com/xiam/fnlang"
+	_ "github.com/xiam/fnlang/stdlib"
+	//"github.com/xiam/sexpr/ast"
+	"github.com/xiam/sexpr/parser"
 	"io"
 	"log"
-	//"os"
 )
 
 func main() {
-	/*
-		buf := bytes.NewBuffer(nil)
-		_, err := io.Copy(buf, os.Stdin)
-		if err != nil {
-			log.Fatal("io.Copy: ", err)
-		}
-		root, err := parser.Parse(buf.Bytes())
-		if err != nil {
-			log.Fatal("parser.Parse: ", err)
-		}
-		_, result, err := fnlang.Eval(root)
-		if err != nil {
-			log.Fatal("fnlang.Eval: ", err)
-		}
-		fmt.Printf("%s\n", result)
-	*/
 	rl := newReadLiner()
 	defer rl.Close()
 
+	quitErr := make(chan error)
+
+	r, w := io.Pipe()
+
+	go func() {
+		root, err := parser.NewStreamer(r)
+		//log.Printf("ROOT: %v, err: %v", root, err)
+		//ast.Print(root)
+		if err != nil {
+			quitErr <- err
+			return
+		}
+		_, res, err := fnlang.Eval(root)
+		if err != nil {
+			quitErr <- err
+			return
+		}
+		fmt.Println(res[0].String())
+		quitErr <- nil
+	}()
+
 	for {
 		s, err := rl.Prompt()
-		log.Printf("in: %v, err: %v", s, err)
-		if errors.Is(err, io.EOF) {
-			return
+		if err != nil {
+			w.CloseWithError(err)
+			break
+		}
+		_, err = w.Write([]byte(s + "\n"))
+		if err != nil {
+			w.CloseWithError(err)
+			break
 		}
 	}
 
+	err := <-quitErr
+	if err != nil {
+		log.Fatalf("fn: %v", err)
+	}
 }
